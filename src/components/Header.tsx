dd50a97403008d85d7a5695d5c5aaff02800c8e6
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-23 15:10:03 +0100

    feat: add loading states

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
 import { useCurrentLesson } from "../store/slices/hooks/currentLesson";
+import { useAppSelector } from "../store";
 
 export function Header() {
   const { currentModule, currentLesson } = useCurrentLesson()
-  
-  if (!currentModule || !currentLesson) {
-    return null
+  const isCourseLoading = useAppSelector(state => state.player.isLoading)
+
+  if (isCourseLoading) {
+    return <h1 className="text-2xl font-bold">Carregando...</h1>
   }
   
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-      <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
+      <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule?.title}"</span>
     </div>
   );
 }
