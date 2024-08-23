11db406ac17dabca9c021a8eb6d61c5cfc2a283a
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-23 13:43:10 +0100

    feat: create next reducer and autoplay and arrive at next lesson

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,11 @@
 import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store'
+import { useDispatch } from 'react-redux'
+import { next } from '../store/slices/player'
 
 export function Video() {
+  const dispatch = useDispatch()
+  
   const lesson = useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
 
@@ -10,6 +14,10 @@ export function Video() {
 
     return currentLesson
   })
+
+  function handlePlayNext() {
+    dispatch(next())
+  }
   
   return (
     <div className="w-full bg-zinc-950 aspect-video">
@@ -17,6 +25,8 @@ export function Video() {
         width="100%"
         height="100%"
         controls
+        playing
+        onEnded={handlePlayNext}
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
