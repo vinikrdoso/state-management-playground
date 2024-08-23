e2046f0361961d04ebcc02ecb7133cf45888274e
Vinícius Cardoso Junqueira <vinikrdoso@hotmail.com>
2024-08-23 14:21:32 +0100

    feat: connect app to api using server-json and create start reducer

--- src/store/slices/hooks/currentLesson.tsx
+++ src/store/slices/hooks/currentLesson.tsx
@@ -4,8 +4,8 @@ export const useCurrentLesson = () => {
   return useAppSelector(state => {
     const { currentModuleIndex, currentLessonIndex } = state.player
 
-    const currentModule = state.player.course.modules[currentModuleIndex]
-    const currentLesson = currentModule.lessons[currentLessonIndex]
+    const currentModule = state.player.course?.modules[currentModuleIndex]
+    const currentLesson = currentModule?.lessons[currentLessonIndex]
 
     return { currentModule, currentLesson }
   })
